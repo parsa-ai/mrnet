8d0b3f3777fb01d268dc68b83f2c741fd4a3566c
parsa <parsaalavi9@gmail.com>
2025-12-05 12:40:42 +0330

    feat: add InfinitySlider component and partner images for enhanced homepage display

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
+import InfinitySlider from "./ui/components/InfinitySlider";
 import Hero from "./ui/home/Hero";
 
 export default function Home() {
+
+  const partners = [
+    "images/partners/b1.png",
+    "images/partners/b2.png",
+    "images/partners/b3.png",
+    "images/partners/b4.png",
+    "images/partners/b5.png",
+    "images/partners/b6.png",
+    "images/partners/b7.png",
+  ];
+
   return (
     <>
       <main>
-        <Hero/>
+        <Hero />
+        <section className="main-container mt-11 ">
+          <p className="text-2xl lg:text-4xl tracking-wider font-normal uppercase">500+ клиентов уже с нами</p>
+          <article>
+            <InfinitySlider partners={partners} />
+          </article>
+        </section>
       </main>
     </>
   );
---
+++ app/ui/components/InfinitySlider.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+
+export default function InfinitySlider({ partners }: { partners: string[] }) {
+    // Duplicate to fix loop warning & make perfect infinite scroll
+    const items = [...partners, ...partners];
+
+    return (
+        <div className="w-full py-6 overflow-hidden">
+            <Swiper
+                modules={[Autoplay]}
+                slidesPerView="auto"
+                spaceBetween={30}
+                loop={true}
+                speed={4000}
+                autoplay={{
+                    delay: 0,
+                    disableOnInteraction: false,
+                }}
+                allowTouchMove={false}
+            >
+                {items.map((src, i) => (
+                    <SwiperSlide
+                        key={i}
+                        className="!w-fit flex items-center justify-center"
+                    >
+                        <img
+                            src={src}
+                            className="h-12 object-contain opacity-80 hover:opacity-100 transition"
+                            alt=""
+                        />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </div>
+    );
+}
